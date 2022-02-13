82fec31e6cd75156b69732fc91ddab1f17840440
Cadar <cadar.gabriel20@gmail.com>
2022-02-13 16:19:33 +0100

    ES Completato

--- main.js
+++ main.js
@@ -112,3 +112,40 @@ const icons = [
 		color: 'blue'
 	}
 ];
+
+
+let iconContainer = document.getElementById("container-icon");
+generaIcone(icons, iconContainer);
+
+let select = document.getElementById("select-type");
+select.addEventListener("change", function () {
+
+	let selezione = this.value;
+
+	const filtrato = icons.filter(icons => {
+
+		if (icons.type == selezione) {
+			return true;
+		}
+		return false;
+	});
+
+	if(selezione == 'all'){
+		generaIcone(icons, iconContainer);
+	} else {
+		generaIcone(filtrato, iconContainer);
+	}
+
+});
+
+function generaIcone(icons, iconContainer) {
+	let contenuto = "";
+	icons.forEach(element => {
+		contenuto += `<div class="icon">
+						 <i style="color:${element.color}" class="${element.family} ${element.prefix}${element.name}"></i>
+						<span>${element.name}</span>
+					  </div>`;
+	});
+	iconContainer.innerHTML = contenuto;
+}
+
